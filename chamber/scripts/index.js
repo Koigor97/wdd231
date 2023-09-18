585473dee810adc1fe0a8397ebb32d3f4b27fe08
Koigor97 <turaysamuel101@yahoo.com>
2023-09-18 21:10:35 +0200

    complete setting up project.

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -3,12 +3,11 @@
 const getNews = async function() {
     const url = './data/event.json';
     const response = await fetch(url);
-    console.log(response);
     const data = await response.json();
     console.log(data);
     
 } 
 
-getNews();
+// getNews();
 
 // data
