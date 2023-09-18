8f68e8a7514bb437559ef883ef97251ae3858b4d
Koigor97 <turaysamuel101@yahoo.com>
2023-09-18 10:57:01 +0200

    Add the files for B.O.M learning activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const input = document.querySelector("#favchap");
+const list = document.querySelector("#list");
+const addBtn = document.querySelector("#btn");
+
+function addChapter() {
+    if (input.value !== "") {
+        let listItem = document.createElement("li");
+
+        let deleteBtn = document.createElement("button")
+        deleteBtn.style.padding = "10px";
+        deleteBtn.style.backgroundColor = "#9e9c48";
+        deleteBtn.style.border= "none";
+        deleteBtn.style.borderRadius = "5px";
+        
+
+
+        listItem.textContent = input.value;
+        deleteBtn.textContent = "❌";
+       
+        deleteBtn.addEventListener("click", () => {
+            list.removeChild(listItem)
+        });
+        
+        listItem.appendChild(deleteBtn);
+        list.appendChild(listItem);
+    }
+    input.value = "";
+    input.focus();
+}
+
+addBtn.addEventListener("click", addChapter)
