5995926003ee876b01ed060cce52a9c6675a7f9a
Koigor97 <turaysamuel101@yahoo.com>
2023-09-28 20:01:11 +0200

    modify the student photo

--- scripts/assignmentPortal.js
+++ scripts/assignmentPortal.js
@@ -29,5 +29,48 @@ const getDate = function () {
   return dateNYear;
 };
 
+
+class Switch {
+  constructor(switchMode) {
+    this.switchBtn = switchMode;
+    this.switchBtn.addEventListener('click', () => this.toggleStatus());
+    this.switchBtn.addEventListener('keydown', (event) =>
+      this.handleKeydown(event)
+    );
+  }
+
+  handleKeydown(event) {
+    // Only do something when space or return is pressed
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      this.toggleStatus();
+    }
+  }
+
+  // Switch state of a switch
+  toggleStatus() {
+    const currentState =
+      this.switchBtn.getAttribute('aria-checked') === 'true';
+    const switchDot = this.switchBtn.querySelector('.switch span');
+    // console.log(currentState);
+    const newState = String(!currentState);
+    // console.log(newState);
+
+    this.switchBtn.setAttribute('aria-checked', newState);
+    switchDot.classList.toggle('moveRight');
+  }
+}
+
+// Initialize switches
+window.addEventListener('load', function () {
+  // Initialize the Switch component on all matching DOM nodes
+  Array.from(document.querySelectorAll('[role^=switch]')).forEach(
+    (element) => {
+      new Switch(element)
+      // console.log(element);
+    }
+  );
+});
+
 year.textContent = getDate().year;
 date.textContent = `${getDate().date} ${getDate().time}`;
