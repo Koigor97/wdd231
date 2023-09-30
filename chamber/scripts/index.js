c38e633c1cfab3e7036d54e2c1e2947ef8eb9b1b
Koigor97 <turaysamuel101@yahoo.com>
2023-09-30 22:59:51 +0200

    making the calendar dynamic

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -5,6 +5,28 @@ const menuBtn = document.querySelector('.menu-btn');
 const theYear = document.querySelector('#year');
 const lastModify = document.querySelector('.last-modify')
 const messageBox =  document.querySelector('.message-box');
+const daysContainer = document.querySelector(".days");
+const nextMonthBtn = document.querySelector(".next-btn");
+const prevMonthBtn = document.querySelector(".prev-btn");
+const month = document.querySelector(".the-date");
+const todayBtn = document.querySelector('.today');
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+] 
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 class Switch {
   constructor(switchMode) {
@@ -101,6 +123,111 @@ nav.addEventListener("click", (e) => {
   }
 
 });
+
+//  getting current date
+const date = new Date();
+// getting the current month
+let currentMonth = date.getMonth();
+// getting the current year
+let currentYear = date.getFullYear();
+
+// function for rendering the days
+function renderCalender() {
+  // getting the previous, current and next month days
+  date.setDate(1);
+  const firstDay = new Date(currentYear, currentMonth, 1);
+  console.log(firstDay.getDay());
+  const lastDay =  new Date(currentYear, currentMonth + 1, 0);
+  console.log(lastDay);
+  const lastDayIndex = lastDay.getDay();
+  console.log(lastDayIndex);
+  const lastDayDate = lastDay.getDate();
+  console.log(lastDayDate);
+  const prevLastDay = new Date(currentYear, currentMonth, 0);
+  console.log(prevLastDay);
+  const prevLastDayDate = prevLastDay.getDate();
+  console.log(prevLastDayDate);
+  const nextDays = (7 - lastDayIndex) - 1;
+  console.log(nextDays);
+
+  // updating the current year and month in header
+  month.innerHTML = `${months[currentMonth]} ${currentYear}`;
+  // updating the days 
+  let days = '';
+  // updating previous days 
+  for (let x = firstDay.getDay(); x > 0; x--) {
+    days +=`<div class="day prev">${prevLastDayDate - x + 1}</div>`;  
+  }
+
+  // current month days
+  for (let i = 1; i <= lastDayDate; i++) {
+    // check if its today then add today class
+    if (i === new Date().getDate() && currentMonth === new Date().getMonth() && currentYear === new Date().getFullYear()) {
+      // if date month year matches add today
+      days += `<div class="day today">${i}</div>`;
+    } else {
+      // else dont add today
+      days += `<div class="day">${i}</div>`;
+    }
+  }
+
+  // next month days
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="day next">${j}</div>`
+  }
+
+  hideTodayBtn();
+  daysContainer.innerHTML = days;
+
+}
+renderCalender();
+
+nextMonthBtn.addEventListener('click', () => {
+  // increase the current month by one
+  currentMonth++
+  if (currentMonth > 11) {
+    // if current month is greater 11, set it zero and increase year by one
+    currentMonth = 0;
+    currentYear++
+  }
+  // render calendar
+  renderCalender();
+})
+
+prevMonthBtn.addEventListener("click", ()=> {
+  // decrease by one
+  currentMonth--;
+  // checking if it's less than 0 set it to 11 and decrease year
+  if (currentMonth < 0) {
+    currentMonth = 11;
+    currentYear--;
+
+    // render calendar
+    renderCalender();
+  }
+
+  // render calendar
+  renderCalender();
+});
+
+// go to today
+todayBtn.addEventListener('click', () => {
+  // set the month and year to current
+  currentMonth =  date.getMonth();
+  currentYear = date.getFullYear();
+
+  // render calendar
+  renderCalender();
+});
+
+// hiding today button if it's already the current date
+function hideTodayBtn() {
+  if (currentMonth === new Date().getMonth() && currentYear === new Date().getFullYear()) {
+    todayBtn.style.display = "none";
+  } else {
+    todayBtn.style.display = "flex";
+  }
+}
 
 const welcomeMsg = `<h1>Welcome! Let us know if you have any questions.</h1>`;
 const welcomeBack = `<h1>Back so soon! Awesome!</h1>`
