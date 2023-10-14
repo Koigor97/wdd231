bfdabd0becab00ba53ebc0a8e0c9ccddaea39a1e
Koigor97 <turaysamuel101@yahoo.com>
2023-10-14 13:32:39 +0200

    modify index.js file

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -10,6 +10,9 @@ const nextMonthBtn = document.querySelector(".next-btn");
 const prevMonthBtn = document.querySelector(".prev-btn");
 const month = document.querySelector(".the-date");
 const todayBtn = document.querySelector('.today');
+const memberCardBox = document.querySelector('.member-card-section');
+const gridBtn = document.getElementById('grid-view');
+const listBtn = document.getElementById('list-view');
 
 const months = [
   "January",
@@ -70,6 +73,68 @@ window.addEventListener('load', function () {
     }
   );
 });
+
+const membersUrl = '../data/members.json';
+
+const getMembersData = async function() {
+  const response = await fetch(membersUrl);
+  const data = await response.json();
+  // console.log(data);
+  renderMembers(data.data);
+}
+
+getMembersData();
+
+const renderMembers = function(members) {
+  members.forEach((member) => {
+    const html = `
+      <div class="member-card">
+          <figure>
+            <div>
+              <img src="${member.image}" alt="${member.name} logo" width="1000" height="623" loading="lazy">
+            </div>
+            <figcaption>${member.name}</figcaption>
+          </figure>
+          <div class="member-info-box">
+              <p>${member.address}</p>
+              <p>${member.phone}</p>
+              <a href="${member.website}">${member.website.slice(8)}</a>
+              <img src="${member.membershipLevel}" alt="" width="45" height="64">
+          </div>
+      </div>
+    `;
+    memberCardBox.insertAdjacentHTML('afterbegin', html)
+  })
+}
+
+const saveView = function(view) {
+  localStorage.setItem("localView", JSON.stringify(view));
+}
+
+const changeView = function() {
+  const btn = this;
+
+  if(memberCardBox.classList.contains('list')) {
+    memberCardBox.classList.remove('list');
+  } else {
+    memberCardBox.classList.remove('grid');
+  }
+  memberCardBox.classList.add(btn.id.slice(0, 4))
+  console.log(btn.id);
+  saveView(btn.id.slice(0, 4));
+}
+
+const theView  = function() {
+  const isView = JSON.parse(localStorage.getItem("localView"));
+
+  if (isView && isView != 0) {
+    memberCardBox.classList.add(isView);
+  }
+}
+
+gridBtn.addEventListener('click', changeView.bind(gridBtn));
+listBtn.addEventListener('click', changeView.bind(listBtn));
+theView();
 
 const getDate = function () {
   const date = new Date();
