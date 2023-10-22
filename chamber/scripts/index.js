667e351d144acf8cf25369ffb636409ade014b65
Koigor97 <turaysamuel101@yahoo.com>
2023-10-22 20:14:52 +0200

    fix homepage, discover page bugs

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -2,34 +2,18 @@
 
 const nav = document.querySelector('.header-nav');
 const menuBtn = document.querySelector('.menu-btn');
-const theYear = document.querySelector('#year');
-const lastModify = document.querySelector('.last-modify')
-const messageBox = document.querySelector('.message-box');
-const daysContainer = document.querySelector(".days");
-const nextMonthBtn = document.querySelector(".next-btn");
-const prevMonthBtn = document.querySelector(".prev-btn");
-const month = document.querySelector(".the-date");
-const todayBtn = document.querySelector('.today');
-const memberCardBox = document.querySelector('.member-card-section');
-const gridBtn = document.getElementById('grid-view');
-const listBtn = document.getElementById('list-view');
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-]
+// const gridBtn = document.getElementById('grid-view');
+// const listBtn = document.getElementById('list-view');
 
-const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+//  getting current date
+const date = new Date();
+// getting the current month
+let currentMonth = date.getMonth();
+// getting the current year
+let currentYear = date.getFullYear()
+
+
 
 class Switch {
   constructor(switchMode) {
@@ -63,7 +47,7 @@ class Switch {
   }
 }
 
-// Initialize switches
+// // Initialize switches
 window.addEventListener('load', function () {
   // Initialize the Switch component on all matching DOM nodes
   Array.from(document.querySelectorAll('[role^=switch]')).forEach(
@@ -74,190 +58,118 @@ window.addEventListener('load', function () {
   );
 });
 
-const toggleMenu = function () {
-  nav.classList.toggle('show-nav')
+// //////////////////////////////////////////////
+function toggleNav() {
+  const toggleMenu = function () {
+    nav.classList.toggle('show-nav')
+  }
+
+  menuBtn.addEventListener('click', toggleMenu);
 }
+toggleNav();
 
-const hoverFadeInOut = function (e) {
-  if (e.target.classList.contains('nav-links')) {
-    const theNav = e.target;
-    const linksList = theNav.closest('.nav-list').querySelectorAll('.nav-links');
 
-    linksList.forEach(theLink => {
-      if (theLink !== theNav) {
-        theLink.style.opacity = this;
+// ///////////////////////////////////////////////
+// The active page functionality - highlight the current  active 
+// page
+function activePage() {
+
+  function setActiveLink() {
+    const navLinks = document.querySelectorAll('.nav-links');
+    const currentURL = window.location.href;
+
+    navLinks.forEach(link => {
+      if (currentURL === link.href) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
       }
     });
   }
+
+  setActiveLink();
+
 }
+activePage();
 
-
-nav.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (e.target.classList.contains('nav-links')) {
-    const links = e.target
-      .closest(".nav-list")
-      .querySelectorAll(".nav-links");
-
-    const theLink = e.target;
-    links.forEach((e) => {
-      e.classList.remove("active");
-    });
-
-    if (theLink.tagName.toLowerCase() !== "a") return;
-    theLink.classList.add("active");
-  }
-
-});
-
-nav.addEventListener('mouseover', hoverFadeInOut.bind(0.5))
-nav.addEventListener('mouseout', hoverFadeInOut.bind(1))
-menuBtn.addEventListener('click', toggleMenu);
+/////////////////// HOME PAGE SECTION ////////////////////
 //////////////////////////////////////////////////////////
+// The weather forecast functionality. Displaying the weather
+// forecast to the user -25.74433236181061, 28.234333823643258
+function weatherForecast() {
+  const lat = -25.74433236181061;
+  const long = 28.234333823643258;
+  const apiKey = 'f4619f75c2d45cc1bfe1a55992e82aaa';
 
-const membersUrl = '../data/members.json';
+  const currentTemp = document.querySelector('#current-temp');
+  const weatherIcon = document.querySelector('#weather-icon');
+  const captionDesc = document.querySelector('#description');
+  const windSpeed = document.querySelector('#windspeed');
+  const windDirection = document.querySelector('#wind-direction');
 
-const getMembersData = async function() {
-  const response = await fetch(membersUrl);
-  const data = await response.json();
-  renderMembers(data.data);
-}
-getMembersData();
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
 
-const renderMembers = function(members) {
-  members.forEach((member) => {
-    const html = `
-      <div class="member-card">
-          <figure>
-            <div>
-              <img src="${member.image}" alt="${member.name} logo" width="1000" height="623" loading="lazy">
-            </div>
-            <figcaption>${member.name}</figcaption>
-          </figure>
-          <div class="member-info-box">
-              <p>${member.address}</p>
-              <p>${member.phone}</p>
-              <a href="${member.website}">${member.website.slice(8)}</a>
-              <img src="${member.membershipLevel}" alt="" width="45" height="64">
-          </div>
-      </div>
-    `;
-    memberCardBox.insertAdjacentHTML('afterbegin', html)
-  })
-}
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`;
 
-const saveView = function(view) {
-  localStorage.setItem("localView", JSON.stringify(view));
-}
-
-const changeView = function() {
-  const btn = this;
-
-  if(memberCardBox.classList.contains('list')) {
-    memberCardBox.classList.remove('list');
-  } else {
-    memberCardBox.classList.remove('grid');
-  }
-  memberCardBox.classList.add(btn.id.slice(0, 4))
-  saveView(btn.id.slice(0, 4));
-}
-
-const theView  = function() {
-  const isView = JSON.parse(localStorage.getItem("localView"));
-
-  if (isView && isView != 0) {
-    memberCardBox.classList.add(isView);
-  }
-}
-
-theView();
-gridBtn.addEventListener('click', changeView.bind(gridBtn));
-listBtn.addEventListener('click', changeView.bind(listBtn));
-
-////////////////////////////////////////////////////////////
-const getDate = function () {
-  const date = new Date();
-  const option = {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-  };
-
-  const dateNYear = {
-    time: date.toTimeString(),
-    year: date.getFullYear(),
-    date: new Intl.DateTimeFormat("en-SA", option).format(date),
-  };
-
-  return dateNYear;
-};
-theYear.textContent = `${getDate().year}`;
-lastModify.textContent = `${getDate().date} - ${getDate().time}`
-
-// /////////////////////////////////////////////////////////
-
-
-
-//  getting current date
-const date = new Date();
-// getting the current month
-let currentMonth = date.getMonth();
-// getting the current year
-let currentYear = date.getFullYear()
-
-// function for rendering the days
-const renderCalender = function() {
-  // getting the previous, current and next month days
-  date.setDate(1);
-  const firstDay = new Date(currentYear, currentMonth, 1);
-  const lastDay = new Date(currentYear, currentMonth + 1, 0);
-  const lastDayIndex = lastDay.getDay();
-  const lastDayDate = lastDay.getDate();
-  const prevLastDay = new Date(currentYear, currentMonth, 0);
-  const prevLastDayDate = prevLastDay.getDate();
-  const nextDays = (7 - lastDayIndex) - 1;
-
-  // updating the current year and month in header
-  month.innerHTML = `${months[currentMonth]} ${currentYear}`;
-  // updating the days 
-  let days = '';
-
-  // updating previous days 
-  for (let x = firstDay.getDay(); x > 0; x--) {
-    days += `<div class="day prev">${prevLastDayDate - x + 1}</div>`;
-  }
-
-  // current month days
-  for (let i = 1; i <= lastDayDate; i++) {
-    // check if its today then add today class
-    if (i === new Date().getDate() && currentMonth === new Date().getMonth() && currentYear === new Date().getFullYear()) {
-      // if date month year matches add today
-      days += `<div class="day today">${i}</div>`;
-    } else {
-      // else dont add today
-      days += `<div class="day">${i}</div>`;
+  const apiFetch = async function () {
+    try {
+      const response1 = await fetch(weatherUrl);
+      if (response1.ok) {
+        const weatherData = await response1.json();
+        // console.log(weatherData);
+        displayResults(weatherData)
+      } else {
+        throw Error(await response1.text());
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
-  // next month days
-  for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="day next">${j}</div>`
+  function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', 'weather-icon');
+    captionDesc.textContent = `${desc}`;
+    windSpeed.textContent = data.wind.speed;
+    windDirection.innerHTML = `${data.wind.deg}&deg;`
   }
-
-  hideTodayBtn();
-  daysContainer.innerHTML = days;
+  apiFetch();
 }
+weatherForecast();
 
-// hiding today button if it's already the current date
-function hideTodayBtn() {
-  if (currentMonth === new Date().getMonth() && currentYear === new Date().getFullYear()) {
-    todayBtn.style.display = "none";
-  } else {
-    todayBtn.style.display = "flex";
+// ////////////////////////////////////////////////////////////
+function displayDate() {
+  const theYear = document.querySelector('#year');
+  const lastModify = document.querySelector('.last-modify');
+
+  const getDate = function () {
+    const date = new Date();
+    const option = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    };
+
+    const dateNYear = {
+      time: date.toTimeString(),
+      year: date.getFullYear(),
+      date: new Intl.DateTimeFormat("en-SA", option).format(date),
+    };
+    return dateNYear;
   }
-}
+  theYear.textContent = `${getDate().year}`;
+  lastModify.textContent = `${getDate().date} - ${getDate().time}`;
+};
+displayDate();
 
+////////////////////// DISCOVER PAGE SECTION /////////////////////
+/////////////////////////////////////////////////////////////
 function feedbackMessage() {
+  const messageBox = document.querySelector('.message-box');
+
   const welcomeMsg = `<p>Welcome! Let us know if you have any questions.</p>`;
   const welcomeBack = `<p>Back so soon! Awesome!</p>`
   const sinceLastVist = `<p>You last visited [n] days ago</p>`;
@@ -275,7 +187,7 @@ function feedbackMessage() {
 
     const daysPast = Math.abs(Math.trunc((currentTime - lastVisit) / millisecToDay));
     // give user feedback based on the days past
-   if (daysPast === 0) {
+    if (daysPast === 0) {
       messageBox.innerHTML = welcomeBack;
     } else {
       messageBox.innerHTML = sinceLastVist.replace('[n]', daysPast);
@@ -286,47 +198,205 @@ function feedbackMessage() {
 
   localStorage.setItem('visitTimeStamp', JSON.stringify(currentTime))
 }
-
-
-
-nextMonthBtn.addEventListener('click', () => {
-  // increase the current month by one
-  currentMonth++
-  if (currentMonth > 11) {
-    // if current month is greater 11, set it zero and increase year by one
-    currentMonth = 0;
-    currentYear++
-  }
-  // render calendar
-  renderCalender();
-});
-
-prevMonthBtn.addEventListener("click", () => {
-  // decrease by one
-  currentMonth--;
-  // checking if it's less than 0 set it to 11 and decrease year
-  if (currentMonth < 0) {
-    currentMonth = 11;
-    currentYear--;
-  }
-  // render calendar
-  renderCalender();
-});
-
-// go to today
-todayBtn.addEventListener('click', () => {
-  // set the month and year to current
-  currentMonth = date.getMonth();
-  currentYear = date.getFullYear();
-  // render calendar
-  renderCalender();
-});
-
 feedbackMessage();
-renderCalender();
+
+// /////////////////////////////////////////////////////////
+// // function for rendering the days in the calendar
+function displayCalender() {
+  const daysContainer = document.querySelector(".days");
+  const nextMonthBtn = document.querySelector(".next-btn");
+  const prevMonthBtn = document.querySelector(".prev-btn");
+  const month = document.querySelector(".the-date");
+  const todayBtn = document.querySelector('.today');
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]
+
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+  const renderCalender = function () {
+    // getting the previous, current and next month days
+    date.setDate(1);
+    const firstDay = new Date(currentYear, currentMonth, 1);
+    const lastDay = new Date(currentYear, currentMonth + 1, 0);
+    const lastDayIndex = lastDay.getDay();
+    const lastDayDate = lastDay.getDate();
+    const prevLastDay = new Date(currentYear, currentMonth, 0);
+    const prevLastDayDate = prevLastDay.getDate();
+    const nextDays = (7 - lastDayIndex) - 1;
+
+    // updating the current year and month in header
+    month.innerHTML = `${months[currentMonth]} ${currentYear}`;
+    // updating the days 
+    let days = '';
+
+    // updating previous days 
+    for (let x = firstDay.getDay(); x > 0; x--) {
+      days += `<div class="day prev">${prevLastDayDate - x + 1}</div>`;
+    }
+
+    // current month days
+    for (let i = 1; i <= lastDayDate; i++) {
+      // check if its today then add today class
+      if (i === new Date().getDate() && currentMonth === new Date().getMonth() && currentYear === new Date().getFullYear()) {
+        // if date month year matches add today
+        days += `<div class="day today">${i}</div>`;
+      } else {
+        // else dont add today
+        days += `<div class="day">${i}</div>`;
+      }
+    }
+
+    // next month days
+    for (let j = 1; j <= nextDays; j++) {
+      days += `<div class="day next">${j}</div>`
+    }
+
+    hideTodayBtn();
+    daysContainer.innerHTML = days;
+  }
+  renderCalender();
+
+  nextMonthBtn.addEventListener('click', () => {
+    // increase the current month by one
+    currentMonth++
+    if (currentMonth > 11) {
+      // if current month is greater 11, set it zero and increase year by one
+      currentMonth = 0;
+      currentYear++
+    }
+    // render calendar
+    renderCalender();
+  });
+
+  prevMonthBtn.addEventListener("click", () => {
+    // decrease by one
+    currentMonth--;
+    // checking if it's less than 0 set it to 11 and decrease year
+    if (currentMonth < 0) {
+      currentMonth = 11;
+      currentYear--;
+    }
+    // render calendar
+    renderCalender();
+  });
+
+  // go to today
+  todayBtn.addEventListener('click', () => {
+    // set the month and year to current
+    currentMonth = date.getMonth();
+    currentYear = date.getFullYear();
+    // render calendar
+    renderCalender();
+  });
+
+  // hiding today button if it's already the current date
+  function hideTodayBtn() {
+    if (currentMonth === new Date().getMonth() && currentYear === new Date().getFullYear()) {
+      todayBtn.style.display = "none";
+    } else {
+      todayBtn.style.display = "flex";
+    }
+  }
+
+}
+displayCalender();
+
+
+////////////////////// DIRECTORY PAGE SECTION /////////////////////
+///////////////////////////////////////////////////////////////
+// Displaying Chamber members and there membership status
+function displayMember() {
+  const membersUrl = '../data/members.json';
+
+  const getMembersData = async function () {
+    const response = await fetch(membersUrl);
+    const data = await response.json();
+    renderMembers(data.data);
+  }
+  getMembersData();
+
+  const renderMembers = function (members) {
+    members.forEach((member) => {
+      const html = `
+      <div class="member-card">
+          <figure>
+            <div>
+              <img src="${member.image}" alt="${member.name} logo" width="1000" height="623" loading="lazy">
+            </div>
+            <figcaption>${member.name}</figcaption>
+          </figure>
+          <div class="member-info-box">
+              <p>${member.address}</p>
+              <p>${member.phone}</p>
+              <a href="${member.website}">${member.website.slice(8)}</a>
+              <img src="${member.membershipLevel}" alt="" width="45" height="64">
+          </div>
+      </div>
+    `;
+      memberCardBox.insertAdjacentHTML('afterbegin', html)
+    })
+  }
+}
+displayMember();
+
+// ///////////////////////////////////////////////////////
+// const saveView = function (view) {
+//   localStorage.setItem("localView", JSON.stringify(view));
+// }
+
+// const changeView = function () {
+//   const btn = this;
+
+//   if (memberCardBox.classList.contains('list')) {
+//     memberCardBox.classList.remove('list');
+//   } else {
+//     memberCardBox.classList.remove('grid');
+//   }
+//   memberCardBox.classList.add(btn.id.slice(0, 4))
+//   saveView(btn.id.slice(0, 4));
+// }
+
+// const theView = function () {
+//   const isView = JSON.parse(localStorage.getItem("localView"));
+
+//   if (isView && isView != 0) {
+//     memberCardBox.classList.add(isView);
+//   }
+// }
+
+// theView();
+// gridBtn.addEventListener('click', changeView.bind(gridBtn));
+// listBtn.addEventListener('click', changeView.bind(listBtn));
+
+
+
+
+// // /////////////////////////////////////////////////////////
+// //  getting current date
+// const date = new Date();
+// // getting the current month
+// let currentMonth = date.getMonth();
+// // getting the current year
+// let currentYear = date.getFullYear()
 
 
 
 
 
 
+
+// feedbackMessage();
+// renderCalender();
